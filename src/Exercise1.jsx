f6262a7467ca06adec05ec31052df517a1d364be
yuwazac <yabdi7544@gmail.com>
2025-09-03 21:04:53 +0800

    exercise 1

---
+++ src/Exercise1.jsx
@@ -0,0 +1,22 @@
+export const UerCard = () => {
+    return(
+        <>
+          <h1>jeo dell</h1>
+          <p>Email:jeodel12@gmail.com</p>
+          <form>
+            <label>username:</label>
+            <input type="text"/><br></br>
+            <label>Email:</label>
+            <input type="email"/>
+
+           
+          </form>
+
+       
+        </>
+       
+    
+    )
+}
+
+export default UerCard;
